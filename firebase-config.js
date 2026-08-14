d80e4e50a51254e841325f59eab6fad940eb5940
Luigysenger <SENGER.LUIGY@GMAIL.COM>
2026-08-14 10:20:27 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  // SUAS CONFIGURAÇÕES ATUAIS DO FIREBASE CONTINUAM AQUI
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "SEU_AUTH_DOMAIN_AQUI",
+  databaseURL: "SEU_DATABASE_URL_AQUI",
+  projectId: "SEU_PROJECT_ID_AQUI",
+  storageBucket: "SEU_STORAGE_BUCKET_AQUI",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID_AQUI",
+  appId: "SEU_APP_ID_AQUI"
 };
 
 const app = initializeApp(firebaseConfig);
